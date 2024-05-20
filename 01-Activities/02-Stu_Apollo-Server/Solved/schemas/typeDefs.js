b252627000e704018d207959e793826cb26aeba6
philliploy <ploy3_98@yahoo.com>
2024-05-20 18:23:01 -0500

    add client

---
+++ 01-Activities/02-Stu_Apollo-Server/Solved/schemas/typeDefs.js
@@ -0,0 +1,16 @@
+const typeDefs = `
+  # Define which fields are accessible from the Class model
+  type Class {
+    _id: ID
+    name: String
+    building: String
+    creditHours: Int
+  }
+
+  # Define which queries the front end is allowed to make and what data is returned
+  type Query {
+    classes: [Class]
+  }
+`;
+
+module.exports = typeDefs;
